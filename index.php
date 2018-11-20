9fa425d3ceb0ceedba395b20ced8104b3ca8de56
root <personne.nico@gmail.com>
2018-11-20 15:23:13 +0100

    edit

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-$name = 'Nicolas';
-echo "Bonjour " . $name . ", comment vas tu ?";
- ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <a href="http://PHP/"><button type="button" class="btn btn-danger" name="button">Accueil</button></a>
+  <?php
+  $name = 'Théodule';
+  echo 'Bonjour'.' '.$name.', comment vas tu ?';
+  ?>
+</body>
+</html>
